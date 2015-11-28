cd1b74a6a292cab2bddcafbbfcff544e31419f0c
Akiomi Kamakura <akiomik@gmail.com>
2015-11-28 15:33:42 +0900

    added current version of stanford corenlp to the dependencies

--- project/StanfordCoreNLPServerBuild.scala
+++ project/StanfordCoreNLPServerBuild.scala
@@ -18,8 +18,16 @@ object StanfordCoreNLPServerBuild extends Build {
         "com.github.finagle" %% "finch-circe" % "0.9.1",
         "io.circe" %% "circe-generic" % "0.2.0",
         "org.spire-math" %% "jawn-argonaut" % "0.8.3",
-        "com.twitter" %% "twitter-server" % "1.15.0"
-      )
+        "com.twitter" %% "twitter-server" % "1.15.0",
+        // "edu.stanford.nlp" % "stanford-corenlp" % "3.5.2", // this version's JSONOutputter has a bug
+        "edu.stanford.nlp" % "stanford-corenlp" % "3.5.2" classifier "models"
+      ),
+      unmanagedJars in Compile <++= baseDirectory map { base =>
+        val baseDirectories    = base / "lib"
+        val stanfordCoreNLPJar = base / "lib" / "CoreNLP" / "build" / "libs" / "CoreNLP-3.4.1.jar"
+        val customJars         = (baseDirectories ** "*.jar") +++ stanfordCoreNLPJar
+        customJars.classpath
+      }
     )
   )
 }
